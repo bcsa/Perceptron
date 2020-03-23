9dd9ca4dbeb3bd0a324e89ecf4caaea78590dab6
Bakos Csongor <csongiika@gmail.com>
2020-03-23 20:54:27 +0200

    Add files via upload

---
+++ perceptron.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([1, x1, x2])
+    w = np.array([-1.5, 1, 1])
+    y = np.sum(w*x)
+    if y <= 0:
+        return 0
+    else:
+        return 1
+
+if __name__ == '__main__':
+    input = [(0, 0), (1, 0), (0, 1), (1, 1)]
+
+    print("AND")
+    for x in input:
+        y = AND(x[0], x[1])
+        print(str(x) + " -> " + str(y))
